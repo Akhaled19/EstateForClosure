87c77b1412f8f7df6f2fbe40454c207801ea477a
Azal Khaled <akhaled@Azals-MacBook-Pro.local>
2026-07-01 18:37:52 -0400

    hiding the Navbar on login and signup pages

--- estate-frontend/src/App.tsx
+++ estate-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/dashboard";
@@ -7,28 +7,30 @@ import Scan from "./pages/scan";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
 
-import { useState } from 'react'
+function Layout() {
+  const location = useLocation();
+  const hideNavbar = ["/login", "/signup"].includes(location.pathname);
 
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/inventory" element={<Inventory />}/>
+          <Route path="/scan" element={<Scan />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp />}/>
+        </Routes>
+    </>
+  )
+}
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar /> 
-
-
-      <Routes>
-
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/scan" element={<Scan />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-
-
-      </Routes>
-
+      <Layout />
     </BrowserRouter>
   );
 }
