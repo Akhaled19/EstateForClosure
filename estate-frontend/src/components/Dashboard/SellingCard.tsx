7133b16c7a791ff2dfe5da702db8d461f9af264e
andy <nsfatster@gmail.com>
2026-07-14 15:29:13 -0400

    Dashboard UI

---
+++ estate-frontend/src/components/Dashboard/SellingCard.tsx
@@ -0,0 +1,64 @@
+export default function SellingCard() {
+  return (
+    <div className="bg-white rounded-xl shadow-sm p-4">
+
+      <h2 className="text-xl font-bold mb-3 text-[rgb(27,42,74)]">
+        Selling Overview
+      </h2>
+
+
+      <div className="flex justify-between py-3 border-b border-gray-200">
+
+        <div className = "card-values">
+          Pending Shipments
+        </div>
+
+        <div className="font-bold">
+          4
+        </div>
+
+      </div>
+
+
+      <div className="flex justify-between py-3 border-b border-gray-200">
+
+        <div className = "card-values">
+          Offers Waiting
+        </div>
+
+        <div className="font-bold">
+          9
+        </div>
+
+      </div>
+
+
+      <div className="flex justify-between py-3 border-b border-gray-200">
+
+        <div className = "card-values">
+          Unsold Listings (30+ days)
+        </div>
+
+        <div className="font-bold">
+          7
+        </div>
+
+      </div>
+
+
+      <div className="flex justify-between py-3">
+
+        <div className = "card-values">
+          Listings Ending Soon
+        </div>
+
+        <div className="font-bold">
+          2
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
--- estate-frontend/src/pages/dashboard.tsx
+++ estate-frontend/src/pages/dashboard.tsx
@@ -1,21 +1,41 @@
+import StatsCard from "../components/Dashboard/StatsCard";
+import Revenue from "../components/Dashboard/Revenue";
+import FinanceCard from "../components/Dashboard/FinanceCard";
+import SellingCard from "../components/Dashboard/SellingCard";
 
 
 export default function Dashboard() {
-
-
   return (
     <div className="dashboard">
+      <div className = "dashboard-wrapper">
+        <div className = "mb-8"> 
+            <h1 className = "text-3xl font-bold"> 
+              Dashboard
+            </h1>
 
-      <div className="content pt-5">
+            <div className = "text-gray-500 mt-2">
+              Here's an general overview of your estate.
+            </div>
 
-        <div className="text-[30px] text-black ml-5">
-          Dashboard:
         </div>
 
+        <div className = "cards"> 
+            <StatsCard name = "Inventory" value = {25} />
+            <StatsCard name = "Listings" value = {9} />
+            <StatsCard name = "Sold" value = {13} />
+            <StatsCard name = "Shipped" value = {5} />
 
+        </div> 
+
+        <Revenue/>
+
+        <div className = "grid grid-cols-2 gap-6 mt-8"> 
+          <FinanceCard/> 
+          <SellingCard/>
+        </div>
 
       </div>
-
+      
     </div>
   );
 }
